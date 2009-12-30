4aad6ad4b63a646a9111d2ee53d8550a7c641e62
Justin Love <hg@JustinLove.name>
2009-12-30 07:02:07 -0600

    test for identifier conflicts and give js priority

--- script/cgd/Module.js
+++ script/cgd/Module.js
@@ -234,7 +234,7 @@ CGD.god = window;
 
   CGD.mod = new CGD.Module('Module', function(m){
     m.root = CGD.Module.pathTo(window.location.toString());
-    m.alreadyNamed('script', 'src');
     m.alreadyNamed('link', 'href');
+    m.alreadyNamed('script', 'src');
   });
 }());
