b910023488ceaa43aa799af11eec53dfcf169164
Justin Love <hg@JustinLove.name>
2009-12-30 06:25:26 -0600

    static files without extension; causes name collisions between types

--- script/cgd/Module.js
+++ script/cgd/Module.js
@@ -193,7 +193,8 @@ CGD.god = window;
         var fullPath = tags[i][attr];
         if (fullPath.indexOf(this.root) == 0) {
           var relativePath = fullPath.substr(this.root.length);
-          new CGD.Dependency(relativePath, tags[i].type).
+          var identifier = relativePath.replace(/\.(\w*)$/, '');
+          new CGD.Dependency(identifier, tags[i].type).
             canonically(fullPath).
             register(this.files).
             loaded();
